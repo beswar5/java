d00336140093052ccdbbe56de3042daa91a0ac93
beswar5 <127486089+beswar5@users.noreply.github.com>
2023-03-10 16:10:14 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,19 @@
+import  java.util.*;
+public class add {
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter a:");
+        a=s.nextInt();
+        System.out.println("enter b:");
+        b=s.nextInt();
+        System.out.println(a+b);
+        //a=10;
+       // b=20;
+       // c=a+b;
+        //System.out.println(c);
+
+
+    }
+}
