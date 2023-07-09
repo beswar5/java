d56d0e2aaeaed36d7a31c77fa9091c4c467f68fd
beswar5 <127486089+beswar5@users.noreply.github.com>
2023-07-09 08:52:30 +0530

    Add files via upload

---
+++ Vowel1.java
@@ -0,0 +1,20 @@
+class  Vowel1
+{
+	public static void main(String[] args) 
+	{
+		char ch='+';
+		if(ch>='a' && ch<='z')
+		{
+			System.out.println(ch+" is a lower case");
+		}
+		else if(ch>='A' && ch<='Z')
+		{
+			System.out.println(ch+" is  upper case");
+		}
+		else
+		{
+			System.out.println(ch+" is a not alphabet");
+		}
+
+	}
+}
